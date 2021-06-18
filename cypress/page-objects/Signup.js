bff8df8471ce2d9bfab109f5c741f9649b836c4c
Alan <alanreskin7@gmail.com>
2021-06-18 13:41:59 -0300

    Added remaning methods to the page object

--- cypress/page-objects/Signup.js
+++ cypress/page-objects/Signup.js
@@ -12,3 +12,22 @@ export function addPassword(password) {
 export function clickSignUpButton() {
   cy.get('.btn').click();
 }
+export function signUpPage() {
+  cy.get(':nth-child(3) > .nav-link').click();
+}
+export function getErrorMessage(position, message) {
+  cy.get(`.error-messages > :nth-child(${position})`).should(
+    'have.text',
+    message
+  );
+}
+export function errorMessageLenghtExpected(lenght) {
+  cy.get('.error-messages li').should('have.length', lenght);
+}
+export function getLoggedUsername(username) {
+  cy.get(':nth-child(4) > .nav-link').should('have.text', username);
+}
+
+export function getUrlAndCompare(url) {
+  cy.url().should('eq', url);
+}
