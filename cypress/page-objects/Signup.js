a798ea4a2628915fd5fd851428fdbfaabe022e10
Alan <alanreskin7@gmail.com>
2021-06-23 12:34:36 -0300

    Fixed corrections of PR

--- cypress/page-objects/Signup.js
+++ cypress/page-objects/Signup.js
@@ -1,33 +1,41 @@
 /// <reference types="cypress" />
 
-export function addEmail(email) {
-  cy.get(':nth-child(2) > .form-control').type(email);
-}
-export function addUsername(username) {
-  cy.get(':nth-child(1) > .form-control').type(username);
-}
-export function addPassword(password) {
-  cy.get(':nth-child(3) > .form-control').type(password);
-}
-export function clickSignUpButton() {
-  cy.get('.btn').click();
-}
-export function signUpPage() {
-  cy.get(':nth-child(3) > .nav-link').click();
-}
-export function getErrorMessage(position, message) {
-  cy.get(`.error-messages > :nth-child(${position})`).should(
-    'have.text',
-    message
-  );
-}
-export function errorMessageLenghtExpected(lenght) {
-  cy.get('.error-messages li').should('have.length', lenght);
-}
-export function getLoggedUsername(username) {
-  cy.get(':nth-child(4) > .nav-link').should('have.text', username);
-}
+export class SignUp {
+  addEmail(email) {
+    cy.get(':nth-child(2) > .form-control').type(email);
+  }
+  addUsername(username) {
+    cy.get(':nth-child(1) > .form-control').type(username);
+  }
+  addPassword(password) {
+    cy.get(':nth-child(3) > .form-control').type(password);
+  }
+  clickSignUpButton() {
+    cy.get('.btn').click();
+  }
+  signUpPage() {
+    cy.get(':nth-child(3) > .nav-link').click();
+  }
+  emailErrorMessage(position) {
+    cy.get(`.error-messages > :nth-child(${position})`).should(
+      'have.text',
+      'email has already been taken'
+    );
+  }
+  usernameErrorMessage(position) {
+    cy.get(`.error-messages > :nth-child(${position})`).should(
+      'have.text',
+      'username has already been taken'
+    );
+  }
+  errorMessageLenghtExpected(lenght) {
+    cy.get('.error-messages li').should('have.length', lenght);
+  }
+  getLoggedUsername(username) {
+    cy.get(':nth-child(4) > .nav-link').should('have.text', username);
+  }
 
-export function getUrlAndCompare(url) {
-  cy.url().should('eq', url);
+  getSignUpUrlAndCompare() {
+    cy.url().should('eq', Cypress.env('REGISTER_URL'));
+  }
 }
